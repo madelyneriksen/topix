83bbc6eb5042d289fd67d1138faa74da6f8aa734
Madelyn Eriksen <hello@madelyneriksen.com>
2019-11-09 13:36:39 -0800

    Added some trivial coverage.

--- topix/utils/tests/test_functional_utilities.py
+++ topix/utils/tests/test_functional_utilities.py
@@ -3,6 +3,8 @@
 
 from unittest import mock
 
+import pytest
+
 from topix.utils import (
     concurrently,
     split_into,
@@ -26,3 +28,22 @@ def test_split_into():
     y.assert_called_once_with("World!")
 
     assert result == x("Hello")
+
+
+def test_consume():
+    """Test consume function exausts iterator."""
+    iterator = iter([1, 2, 3, 4])
+    consume(iterator)
+
+    with pytest.raises(StopIteration):
+        next(iterator)
+
+
+@mock.patch("topix.utils.redis")
+def test_connection_creator_is_singleton(redis_mock):
+    """Test the connection creator doesn't create duplicate clients."""
+    one = connection()
+    two = connection()
+
+    assert one is two
+    redis_mock.Redis.from_url.assert_called_once_with("redis://localhost:6379/0")
